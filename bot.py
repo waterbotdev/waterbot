398b0e5caf5e27ece5ad8cb99d35c9aa370ea2cf
austiko <austinh92102@gmail.com>
2019-12-14 23:51:48 -0500

    bunj!

--- bot.py
+++ bot.py
@@ -104,6 +104,13 @@ async def dog(ctx):
       embed.set_image(url=url)
       embed.set_footer(text="Powered by thedogapi.com!")                                                              
     await ctx.send(embed=embed)
-    
+
+@bot.command()
+async def bunj(ctx):     
+    embed=discord.Embed(title="bunj", url="https://cdn.discordapp.com/attachments/583070530706604034/655630309290934273/Snapchat-446272952.jpg", description="bunj", color=0x8cff8f)
+    embed.set_author(name="bunj", url="https://cdn.discordapp.com/attachments/583070530706604034/655630309290934273/Snapchat-446272952.jpg",, icon_url="https://cdn.discordapp.com/attachments/583070530706604034/655630309290934273/Snapchat-446272952.jpg")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/583070530706604034/655630309290934273/Snapchat-446272952.jpg")
+    embed.add_field(name="bunj", value="bunj", inline=False)
+    await self.bot.say(embed=embed)
 
 bot.run(token)
