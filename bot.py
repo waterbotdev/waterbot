3b9f6554a147637fb2d144aa80acc55ad7069bce
kcomain <cheunghowui@gmail.com>
2019-12-22 18:10:03 +0800

    some update

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ async def userinfo(ctx,member:discord.Member=None):
     embed.add_field(name="Created at:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
     embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
     embed.add_field(name=f"Roles({len(roles)})", value=" ".join([role.mention for role in roles]))
-    embed.add_field(name="Top Role:", value=[role.mention for role in [role for role in author.roles]][len([role.mention for role in [role for role in author.roles]])-1])
+    embed.add_field(name="Top Role:", value=[role.mention for role in [role for role in member.roles]][len([role.mention for role in [role for role in member.roles]])-1])
     embed.add_field(name="Is Bot?",  value=member.bot)
 
     await ctx.send(embed=embed)
