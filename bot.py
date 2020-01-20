220b11e80b82417ed46b6cbab6d9a5a099d15718
Kenny <cheunghowui@gmail.com>
2020-01-20 17:40:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def on_member_join(member):
                                               description=f'Welcome {member.name} to {member.guild.name}.'
                                                           f'There are now {len(member.guild.members)} members.',
                                               color=0xffffff)
-                        channel.send(embed=embed)
+                        await channel.send(embed=embed)
                     except Exception as e:
                         bot.get_channel(668447749443813416).send(f'Error in event on_member_join'
                                                                  f'Server: {member.guild.name}({member.guild.id})'
