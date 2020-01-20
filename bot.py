2e4d614f03e0bade4fa7aff498e7722765dc6f40
Kenny <cheunghowui@gmail.com>
2020-01-20 17:49:00 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,10 +47,10 @@ async def on_member_join(member):
                     channel = bot.get_channel(guildconf[i]['joinchannel'])
                     try:
                         embed = discord.Embed(title='Welcome new member!',
-                                              description=f'Welcome {member.name} to {member.guild.name}.'
+                                              description=f'Welcome {member.name} to {member.guild.name}.\n'
                                                           f'There are now {len(member.guild.members)} members.',
                                               color=0xffffff)
-                        awat channel.send(embed=embed)
+                        channel.send(embed=embed)
                     except Exception as e:
                         await bot.get_channel(668447749443813416).send(f'Error in event on_member_join'
                                                                  f'Server: {member.guild.name}({member.guild.id})'
