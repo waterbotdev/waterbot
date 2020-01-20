7c392347b179a11c9b048f5723f82d535919a248
Kenny <cheunghowui@gmail.com>
2020-01-20 18:09:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,9 +50,27 @@ async def on_member_join(member):
                                               description=f'Welcome {member.name} to {member.guild.name}.\n'
                                                           f'There are now {len(member.guild.members)} members.',
                                               color=0xffffff)
-                        channel.send(embed=embed)
+                        await channel.send(embed=embed)
                     except Exception as e:
                         await bot.get_channel(668447749443813416).send(f'Error in event on_member_join'
+                                                                 f'Server: {member.guild.name}({member.guild.id})'
+                                                                 f'Messag: {e}')
+
+@bot.event
+async def on_member_remove(member):
+    for i in guildconf:
+        if member.guild.id == int(i):
+            if 'leavechannel' in guildconf[i]:
+                if guildconf[i]['leavechannel'] is not None:
+                    channel = bot.get_channel(guildconf[i]['leavechannel'])
+                    try:
+                        embed = discord.Embed(title='Welcome new member!',
+                                              description=f'Bye bye {member.name}.\n'
+                                                          f'There are now {len(member.guild.members)} members.',
+                                              color=0xffffff)
+                        await channel.send(embed=embed)
+                    except Exception as e:
+                        await bot.get_channel(668447749443813416).send(f'Error in event on_member_leave'
                                                                  f'Server: {member.guild.name}({member.guild.id})'
                                                                  f'Messag: {e}')
 
