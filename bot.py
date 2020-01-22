3d936fb64bab9f9d3caa9a07f839cee1a34266b7
kcomain <cheunghowui@gmail.com>
2020-01-22 19:15:18 +0800

    B

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def on_member_join(member):
         if member.guild.id == int(i):
             if 'joinchannel' in guildconf[i]:
                 if guildconf[i]['joinchannel'] is not None:
-                    channel = bot.get_channel(guildconf[i]['joinchannel'])
+                    channel = bot.get_channel(guildconf[i]['joinlogs']['joinchannel'])
                     try:
                         embed = discord.Embed(title='Welcome new member!',
                                               description=f'Welcome **{member.name}** to **{member.guild.name}**.\n'
@@ -64,7 +64,7 @@ async def on_member_remove(member):
         if member.guild.id == int(i):
             if 'leavechannel' in guildconf[i]:
                 if guildconf[i]['leavechannel'] is not None:
-                    channel = bot.get_channel(guildconf[i]['leavechannel'])
+                    channel = bot.get_channel(guildconf[i]['joinlogs']['leavechannel'])
                     try:
                         embed = discord.Embed(title='Goodbye',
                                               description=f'Bye bye **{member.name}**.\n'
