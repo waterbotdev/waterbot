c00cde0b5a8ecac8c67583672e8aba26727cdcec
kcomain <cheunghowui@gmail.com>
2020-02-17 22:53:06 +0800

    mmmmh

--- exts/mod.py
+++ exts/mod.py
@@ -14,9 +14,8 @@ class ModExceptions:
 # Converters?
 
 def can_execute_action(ctx, user, target):
-    return user.id == ctx.bot.owner_id or \
-           user == ctx.guild.owner or \
-           user.top_role > target.top_role
+    #      ||         is_Owner         ||      is_Guild_Owner     ||           RoleHigher          |
+    return user.id == ctx.bot.owner_id or user == ctx.guild.owner or user.top_role > target.top_role
 
 
 async def resolve_member(guild, member_id):
