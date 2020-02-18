f076838d6dbd8f44b6cc23c851764b0039c4f849
Drago <59109582+dragifey@users.noreply.github.com>
2020-02-18 18:59:45 +0100

    Update utils.py

--- exts/utils.py
+++ exts/utils.py
@@ -57,13 +57,13 @@ class Utils(commands.Cog):
         embed.add_field(name="Activity", value=member.activity.name, inline=True)
         embed.add_field(name="Animated Avatar", value=member.is_avatar_animated(), inline=True)
         embed.add_field(name="Avatar URL", value=f"[Click here]({ctx.author.avatar_url})", inline=True)
-        roles = []
-        for role in user.roles:
-            if role.name == "@everyone":
-                pass
-            else:
-                roles.append(role.mention)
-        embed.add_field(name="Roles", value=", ".join(roles), inline=True)
+        #roles = []
+        #for role in user.roles:
+            #if role.name == "@everyone":
+                #pass
+            #else:
+                #roles.append(role.mention)
+        #embed.add_field(name="Roles", value=", ".join(roles), inline=True)
         embed.add_field(name="Top Role:", value=[role.mention for role in [role for role in member.roles]][
             len([role.mention for role in [role for role in member.roles]]) - 1])
         if member.status == discord.Status.online:
