d601d365014016c9286a22b0f4066736a2d93ebc
Drago <59109582+dragifey@users.noreply.github.com>
2020-02-18 16:51:26 +0100

    Update fun.py

--- exts/fun.py
+++ exts/fun.py
@@ -80,7 +80,7 @@ class Fun(commands.Cog):
         Sometimes you gotta humiliate your friends and feed them beans.
         bean [user] <reason>
         Send messages'''
-        await ctx.send(f"{user.name} `({user.id})` has been beaned. Reason `{reason}`")
+        await ctx.send(f"{user} `({user.id})` has been beaned. Reason `{reason}`")
 
     # @bean.error
     # async def beanerror(self, ctx):
